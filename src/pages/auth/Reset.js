05dae69c4f1d1c817d53748c0e5a7af448b933d3
Almir Motika <Almir_18B@hotmail.com>
2023-02-22 02:43:47 +0100

    12. Reset Page JSX

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import styles from "./auth.module.scss";
+import { Card } from '../../components';
+import resetImg from "../../assets/forgot.png";
+import { Link } from 'react-router-dom';
 const Reset = () => {
   return (
-    <div>
-      Reset
-    </div>
+    <section className={`container ${styles.auth}`}>
+      
+      <Card>
+      <div className={styles.form}>
+        <h2>Reset Password</h2>
+        <form>
+          <input type="text" placeholder="Email" required/>
+          <button className="--btn --btn-primary --btn-block">Reset</button>
+          <div className={styles.links}>
+            <p><Link to="/login" >-Login</Link></p>
+            <p><Link to="/register" >-Register</Link></p>
+          </div>
+        </form>
+      </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={resetImg} alt="Register" width="400"/>
+      </div>
+      
+    </section>
   )
 }
 
