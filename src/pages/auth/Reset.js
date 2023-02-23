f35ab4864d3da8d23102c93f5958635e96056194
Almir Motika <Almir_18B@hotmail.com>
2023-02-23 14:06:17 +0100

    19. Reset Password

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.scss";
 import { Card } from '../../components';
 import resetImg from "../../assets/forgot.png";
 import { Link } from 'react-router-dom';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../firebase/config';
+import { toast } from 'react-toastify';
+import Loader from '../../components/loader/Loader';
 const Reset = () => {
+  const [email,setEmail]=useState("");
+  const [isLoading,setIsLoading]=useState(false);
+
+  const resetPassword=(e)=>{
+    e.preventDefault();
+    setIsLoading(true);
+    sendPasswordResetEmail(auth, email)
+  .then(() => {
+    setIsLoading(false);
+    toast.success("Check your email for a reset link");
+  })
+  .catch((error) => {
+    setIsLoading(false);
+    toast.error(error.message);
+  });
+
+  }
   return (
+    <>
+    {isLoading && <Loader/>}
     <section className={`container ${styles.auth}`}>
       
       <Card>
       <div className={styles.form}>
         <h2>Reset Password</h2>
-        <form>
-          <input type="text" placeholder="Email" required/>
-          <button className="--btn --btn-primary --btn-block">Reset</button>
+        <form onSubmit={resetPassword}>
+          <input 
+          type="text" 
+          placeholder="Email" 
+          required
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          />
+          <button type="submit" className="--btn --btn-primary --btn-block">Reset</button>
           <div className={styles.links}>
             <p><Link to="/login" >-Login</Link></p>
             <p><Link to="/register" >-Register</Link></p>
@@ -25,6 +54,7 @@ const Reset = () => {
       </div>
       
     </section>
+    </>
   )
 }
 
