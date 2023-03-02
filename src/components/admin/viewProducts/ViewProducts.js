987424865bed790afdb823435d9697354c423c3e
Almir Motika <Almir_18B@hotmail.com>
2023-03-02 17:12:45 +0100

    41. Get products from firestore

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+import { db } from '../../../firebase/config';
+import styles from "./ViewProducts.module.scss";
 const ViewProducts = () => {
+  const[products,setProducts]=useState([]);
+  const[isLoading,setIsLoading]=useState(false);
+useEffect(()=>{
+  getProducts();
+},[]);
+  const getProducts=()=>{
+    setIsLoading(true);
+    try{
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt","desc"));
+      
+onSnapshot(q, (snapshot) => {
+  //console.log(snapshot.docs);
+  const allProducts=snapshot.docs.map((doc)=>({
+    id:doc.id,
+    ...doc.data()
+  }))
+  console.log(allProducts);
+  setProducts(allProducts);
+  });
+
+    }
+    catch(error){
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  }
   return (
+
     <div>
       ViewProducts
     </div>
