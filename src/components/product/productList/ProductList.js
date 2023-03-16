865b8b47de9c5a08b241971d204365453678f268
Almir Motika <almir.motika@edu.fit.ba>
2023-03-16 12:59:34 +0100

    53. ProductList Component

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./ProductList.module.scss";
+import {BsFillGridFill} from "react-icons/bs";
+import { FaListAlt } from 'react-icons/fa';
 
 const ProductList = () => {
+  const [grid,setGrid]=useState(true);
   return (
-    <div>
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill size={22} color="orangered"
+          onClick={()=>setGrid(true)}/>
+          <FaListAlt size={24} color="#0066d4"
+          onClick={()=>setGrid(false)}/>
+
+          <p>
+            <b>10</b> Products found
+          </p>
+        </div>
+        {/*Search*/}
+          <div>
+            <p>Search</p>
+          </div>
+          {/*Sort*/}
+          <div className={styles.sort}>
+            <label>Sort by:</label>
+            <select>
+              <option value="latest">Latest</option>
+              <option value="lowest-price">Lowest Price</option>
+              <option value="highest-price">Highest Price</option>
+              <option value="a-z">a-z</option>
+              <option value="z-a">z-a</option>
+
+            </select>
+        </div>
+      </div>
       ProductList
     </div>
   )
